b5a072e49836ae0deee95e6d0ba10775810230e2
Ben Cartwright <ben.cartwright@outlook.com>
2022-02-15 12:01:56 -0500

    completed styling for website

--- main.js
+++ main.js
@@ -1,61 +1,45 @@
-const API_URL = "https://api.nasa.gov/planetary/apod?api_key=AEp43Nh1HaBkWgmooQhPNAcumFH5EqfncAcUBMB4";
+const API_URL =
+  "https://api.nasa.gov/planetary/apod?api_key=AEp43Nh1HaBkWgmooQhPNAcumFH5EqfncAcUBMB4";
+function callApi(url) {
+  axios
+    .get(url)
+    .then((result) => {
+      console.log("Successful response", result);
 
-const dateInput = document.getElementById("chosen__date");
+      const imgTitle = result.data.title;
+      const imgDate = result.data.date;
+      const imgAuthor = result.data.copyright || "No author specified";
+      const imgCopy = result.data.explanation;
+      const imgLink = result.data.hdurl;
 
-function api() {
-    form.addEventListener("submit", (event) => {
-      event.preventDefault();
-      //searchResultsEl.innerHTML = "";
-      const searchTerm = event.target.date.value;
-      axios
-      .get(`${API_URL}&date=${searchTerm}`)
-      .then((result) => {
-          console.log("Successful response", result);
-          
-          const imgTitle = result.data.title;
-          const imgDate = result.data.date;
-          const imgAuthor = result.data.copyright || "No author specified";
-          const imgCopy = result.data.explanation;
-          const imgLink = result.data.hdurl;
-          
-          const mainElement = document.getElementById("main")
-          const mainTitle = document.querySelector(".main__title").innerText = imgTitle
-          const mainDate = document.querySelector(".main__date").innerText = imgDate
-          const mainAuthor = document.querySelector(".main__author").innerText = imgAuthor
-          const mainCopy = document.querySelector(".main__copy").innerText = imgCopy
-          const mainImg = document.querySelector(".main__left").src= imgLink
-        })
-        .catch((error) => {
-            console.log("Unsuccessful response", error);
-        });
+      const mainTitle = (document.querySelector(".main__title").innerText =
+        imgTitle);
+      const mainDate = (document.querySelector(".main__date").innerText =
+        imgDate);
+      const mainAuthor = (document.querySelector(".main__author").innerText =
+        imgAuthor);
+      const mainCopy = (document.querySelector(".main__copy").innerText =
+        imgCopy);
+      const mainImg = (document.querySelector(".main__left").src = imgLink);
+    })
+    .catch((error) => {
+      console.log("Unsuccessful response", error);
     });
 }
 
-const todayApi = ()=>{
-    axios
-      .get(`${API_URL}`)
-      .then((result) => {
-        console.log("Successful response", result);
-  
-        const imgTitle = result.data.title;
-        const imgDate = result.data.date;
-        const imgAuthor = result.data.copyright || "No author specified";
-        const imgCopy = result.data.explanation;
-        const imgLink = result.data.hdurl;
-  
-  
-  
-        const mainElement = document.querySelector(".main");
-        const mainTitle = document.querySelector(".main__title").innerText = imgTitle;
-        const mainDate = document.querySelector(".main__date").innerText = imgDate;
-        const mainAuthor = document.querySelector(".main__author").innerText = imgAuthor;
-        const mainCopy = document.querySelector(".main__copy").innerText = imgCopy;
-        const mainImg = document.querySelector(".main__left").src = imgLink;
-  
-      })
-      .catch((error) => {
-        console.log("Unsuccessful response", error);
-      });
-  }
-  
-  api() || todayApi()
+const todayApi = () => {
+  callApi(API_URL);
+};
+
+function api() {
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    //searchResultsEl.innerHTML = "";
+    const searchTerm = event.target.date.value;
+    const newApi = `${API_URL}&date=${searchTerm}`;
+    callApi(newApi);
+    event.target.reset();
+  });
+}
+
+api() || todayApi();
