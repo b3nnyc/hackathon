1d8dbe7eec4f14f7cfccb94e2ddef662bfe66c54
Ben Cartwright <ben.cartwright@outlook.com>
2022-02-15 01:09:07 -0500

    completed js

--- main.js
+++ main.js
@@ -31,4 +31,31 @@ function api() {
     });
 }
 
-api();
+const todayApi = ()=>{
+    axios
+      .get(`${API_URL}`)
+      .then((result) => {
+        console.log("Successful response", result);
+  
+        const imgTitle = result.data.title;
+        const imgDate = result.data.date;
+        const imgAuthor = result.data.copyright || "No author specified";
+        const imgCopy = result.data.explanation;
+        const imgLink = result.data.hdurl;
+  
+  
+  
+        const mainElement = document.querySelector(".main");
+        const mainTitle = document.querySelector(".main__title").innerText = imgTitle;
+        const mainDate = document.querySelector(".main__date").innerText = imgDate;
+        const mainAuthor = document.querySelector(".main__author").innerText = imgAuthor;
+        const mainCopy = document.querySelector(".main__copy").innerText = imgCopy;
+        const mainImg = document.querySelector(".main__left").src = imgLink;
+  
+      })
+      .catch((error) => {
+        console.log("Unsuccessful response", error);
+      });
+  }
+  
+  api() || todayApi()
